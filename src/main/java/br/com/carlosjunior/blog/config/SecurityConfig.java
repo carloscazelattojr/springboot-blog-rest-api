026c1faf3d8d03a89a8ee34bd98ea8b90e54a5f9
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-11-01 07:41:56 -0300

    Implemented login Security

--- src/main/java/br/com/carlosjunior/blog/config/SecurityConfig.java
+++ src/main/java/br/com/carlosjunior/blog/config/SecurityConfig.java
@@ -1,18 +1,17 @@
 package br.com.carlosjunior.blog.config;
 
+import br.com.carlosjunior.blog.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
@@ -20,8 +19,11 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
     @Bean
-    PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
@@ -34,15 +36,20 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic()
-            ;
+                .httpBasic();
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    /*@Override
     @Bean
     protected UserDetailsService userDetailsService(){
         UserDetails user =  User.builder().username("carlos").password(passwordEncoder().encode("123")).roles("USER").build();
         UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
-    }
+    }*/
 }
---
+++ src/main/java/br/com/carlosjunior/blog/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.carlosjunior.blog.repository;
+
+import br.com.carlosjunior.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    //Optional<User> findByEmail(String email);
+    //Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    //Boolean existsByUserName(String username);
+    //Boolean existsByUserEmail(String email);
+}
