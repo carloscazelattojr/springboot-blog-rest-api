524c28e5735f971018a34aec31338a132f9b3b03
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-11-01 06:36:26 -0300

    Created entity User, Role and User_Role Jpa

---
+++ src/main/java/br/com/carlosjunior/blog/entity/Role.java
@@ -0,0 +1,20 @@
+package br.com.carlosjunior.blog.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 60)
+    private String name;
+}
