730835b58ee5be2974ae9ccd27900717d05e11eb
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-11-07 07:06:11 -0300

    Swagger: docs @Api,ApiModel,ApiProperty

--- src/main/java/br/com/carlosjunior/blog/payload/CommentDto.java
+++ src/main/java/br/com/carlosjunior/blog/payload/CommentDto.java
@@ -1,23 +1,30 @@
 package br.com.carlosjunior.blog.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@ApiModel(description = "Comment model information")
 @Data
 public class CommentDto {
 
+    @ApiModelProperty(value = "Blog comment id")
     private Long id;
 
+    @ApiModelProperty(value = "Blog comment Name")
     @NotEmpty(message = "Name should not be null or empty")
     private String name;
 
+    @ApiModelProperty(value = "Blog comment E-mail")
     @NotEmpty(message = "Email shoul not be null or empty")
     @Email
     private String email;
 
+    @ApiModelProperty(value = "Blog comment Body")
     @NotEmpty
     @Size(min = 10, message = "Comment body must be minimum 10 caracters")
     private String body;
--- src/main/java/br/com/carlosjunior/blog/payload/PostDto.java
+++ src/main/java/br/com/carlosjunior/blog/payload/PostDto.java
@@ -1,24 +1,31 @@
 package br.com.carlosjunior.blog.payload;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
+@ApiModel(description = "Post model information")
 @Data
 public class PostDto {
 
+    @ApiModelProperty(value = "Blog post id")
     private Long id;
 
+    @ApiModelProperty(value = "Blog post Title")
     @NotEmpty
     @Size(min = 2, message = "Post title should have at least 2 characteres")
     private String title;
 
+    @ApiModelProperty(value = "Blog post Description")
     @NotEmpty
     @Size(min = 2, message = "Post description should have at least 10 characteres")
     private String description;
 
+    @ApiModelProperty(value = "Blog post Content")
     @NotEmpty
     private String content;
 
